0d0613578679bd7037c6bf7df7c7b27615cfe6fa
dp-qb <836185869@qq.com>
2021-05-13 17:25:02 +0800

    换图标

--- app/build/gen/com/pic/R.java
+++ app/build/gen/com/pic/R.java
@@ -12,37 +12,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int but_90=0x7f020000;
-        public static final int pic1=0x7f020001;
-        public static final int pic2=0x7f020002;
-        public static final int pic3=0x7f020003;
-        public static final int pic4=0x7f020004;
-        public static final int pic6=0x7f020005;
-        public static final int pic_ic=0x7f020006;
+        public static final int ic_launcher=0x7f020001;
+        public static final int pic1=0x7f020002;
+        public static final int pic2=0x7f020003;
+        public static final int pic3=0x7f020004;
+        public static final int pic4=0x7f020005;
+        public static final int pic6=0x7f020006;
+        public static final int pic_ic=0x7f020007;
     }
     public static final class id {
-        public static final int ban_h=0x7f070004;
-        public static final int ban_w=0x7f070005;
-        public static final int bao=0x7f070003;
-        public static final int che=0x7f070001;
-        public static final int dpi=0x7f07000a;
-        public static final int huan_chun=0x7f07000b;
-        public static final int huan_chun_b=0x7f07000c;
-        public static final int jian_l=0x7f070009;
-        public static final int jian_up=0x7f070008;
-        public static final int l=0x7f070007;
-        public static final int ren=0x7f070002;
-        public static final int text=0x7f070000;
-        public static final int up=0x7f070006;
+        public static final int che=0x7f050001;
+        public static final int ren=0x7f050002;
+        public static final int text=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int set=0x7f030001;
-    }
-    public static final class string {
-        public static final int app_name=0x7f050000;
-    }
-    public static final class style {
-        public static final int AppTheme=0x7f060000;
     }
     public static final class xml {
         public static final int file_paths=0x7f040000;
